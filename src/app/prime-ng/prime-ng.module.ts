e2ee0f25671deb2c1be89b5ac0d281a79a51b4a0
abarrios0209 <80713264+abarrios0209@users.noreply.github.com>
2021-06-28 12:37:53 -0500

    Inicio del menubar

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+
+import {MenubarModule} from 'primeng/menubar';
+import {ButtonModule} from 'primeng/button';
+
+
+@NgModule({
+  exports: [
+    MenubarModule,
+    ButtonModule
+  ]
+})
+export class PrimeNgModule { }
